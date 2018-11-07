cbc3caf430785032c074803506e9c6fba43bc69c
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-11-07 18:02:24 -0500

    Begin creating PokemonIndex

---
+++ pokedex/frontend/components/pokemon/pokemon_index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class PokemonIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // render() {
+  //   const all_pokemon =
+  //   return (
+  //     <ul>
+  //       {all_pokemon}
+  //     </ul>
+  //   );
+  // }
+}
--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+import { fetchAllPokemon } from './util/api_util';
+import { configureStore } from './store/store';
+import { selectAllPokemon } from './reducers/selectors';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.getState = store.getState;
+  window.selectAllPokemon = selectAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
+  window.dispatch = store.dispatch;
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
   const rootEl = document.getElementById('root');
-  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  ReactDOM.render(<Root store={store}/>, rootEl);
 });
